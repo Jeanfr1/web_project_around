3f8a4fdbdb06733ae8edb9a9af217233fb31d2ef
Jean <agencejaraujo@gmail.com>
2024-05-23 17:59:19 +0200

    correçoes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  // ... configurações anteriores
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+    clean: true,
+  },
+  target: ["web", "es5"],
+  stats: { children: true },
   mode: "development",
   devServer: {
-    static: path.resolve(__dirname, "./dist"), // especifica uma pasta de onde aplicativo e seu conteúdo será servido
-    compress: true, // isso vai agilizar o carregamento do arquivo no modo de desenvolvimento
-    port: 8080, // abrirá seu site no localhost:8080 (você pode usar outra porta)
-    open: true, // o site abrirá automaticamente no navegador depois de executar npm run dev
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
